9d1e809fdcd14cbef7f4a2f918982ef6f7c5b62e
Sebastian de Castelberg <sebu@kpricorn.org>
2013-09-01 20:57:12 +0200

    implement dropdown element nesting

--- lib/foundation-navigation/dropdown.rb
+++ lib/foundation-navigation/dropdown.rb
@@ -1,10 +1,31 @@
 require_relative './builder'
+require_relative './menu_item'
+require_relative './divider'
 
 module FoundationNavigation
 
   class Dropdown < Builder
-    def initialize(title, link = '#')
-      li(class: 'has-dropdown') { a(href: link) { title }}
+    def build_subtree(klass, *args)
+      @node.children << klass.new(*args).node
+    end
+
+    def divider
+      build_subtree(Divider)
+    end
+
+    def menu_item(title, link, &block)
+      build_subtree(MenuItem, title, link)
+    end
+
+    def initialize(title, link = '#', &block)
+      li(class: 'has-dropdown') do
+        a(href: link) { title }
+        ul(class: 'dropdown') do
+          if block_given?
+            instance_eval(&block)
+          end
+        end
+      end
     end
   end
 
--- spec/lib/foundation-navigation/dropdown_spec.rb
+++ spec/lib/foundation-navigation/dropdown_spec.rb
@@ -5,6 +5,7 @@ require 'foundation-navigation/dropdown'
 # <li class="has-dropdown not-click">
 #   <a href="#">Library</a>
 #   <ul class="dropdown">
+#    <li><a href="http://www.forrst.com">Forrst</a></li>
 #     ...
 #   </ul>
 # </li>
@@ -13,15 +14,48 @@ module FoundationNavigation
 
   describe Dropdown do
     describe '#new' do
-      context 'with title' do
-        it 'renders the li.has-dropdown > a' do
-          expect(Dropdown.new('Library').to_s).to have_tag('li.has-dropdown > a', text: 'Library')
-        end
+      it 'renders li.has-dropdown > a' do
+        expect(
+          Dropdown.new('Library').to_s
+        ).to have_tag('li.has-dropdown > a', text: 'Library')
+      end
+
+      it 'renders li.has-dropdown > ul.dropdown' do
+        expect(
+          Dropdown.new('Library').to_s
+        ).to have_tag('li.has-dropdown > ul.dropdown')
       end
 
       context 'with link' do
         it 'renders the href attribute' do
-          expect(Dropdown.new('Library', '/library').to_s).to have_tag('li.has-dropdown > a', with: {href: '/library'})
+          expect(
+            Dropdown.new('Library', '/library').to_s
+          ).to have_tag('li.has-dropdown > a', with: {href: '/library'})
+        end
+      end
+
+      context 'with menu_item block' do
+        let :dropdown do
+          Dropdown.new('Library') do
+            menu_item('Link 1', '/link1')
+            divider
+            menu_item('Link 2', '/link2')
+          end.to_s
+        end
+
+        it 'renders the nested menu element' do
+          expect(dropdown).to have_tag(
+              'li.has-dropdown > ul.dropdown > li > a',
+              text: 'Link 1',
+              with: { href: '/link1' }
+            )
+        end
+
+        it 'renders the nested divider' do
+          expect(dropdown).to have_tag(
+              'li.has-dropdown > ul.dropdown > li',
+              with: { class: 'divider' }
+            )
         end
       end
     end
